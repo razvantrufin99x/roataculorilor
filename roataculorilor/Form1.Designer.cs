ff62a908989efea60bc6418686aca589332b9f6f
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-02 00:05:47 +0200

    Add project files.

---
+++ roataculorilor/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace roataculorilor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new graphiccontrol.UserControl1();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.White;
+            this.userControl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userControl11.Location = new System.Drawing.Point(3, 4);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(256, 256);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.userControl11_MouseDoubleClick);
+            this.userControl11.MouseDown += new System.Windows.Forms.MouseEventHandler(this.userControl11_MouseDown);
+            this.userControl11.MouseMove += new System.Windows.Forms.MouseEventHandler(this.userControl11_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(266, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(34, 39);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(266, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(34, 39);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(305, 263);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private graphiccontrol.UserControl1 userControl11;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ roataculorilor/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using graphiccontrol;
+using System.Threading;
+
+namespace roataculorilor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Color curentcolor;
+        public Color selectedcolor;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void stilccw1()
+        {
+            for (int i = 0; i < 256; i += 7)
+            {
+                for (int j = 0; j < 256; j += 7)
+                {
+                    for (int k = 0; k < 256; k += 7)
+                    {
+                        try
+                        {
+                            userControl11.g.FillRectangle(new SolidBrush(Color.FromArgb(i, j, k)), i, j, 6, 6);
+                        }
+                        catch { }
+                        Thread.Sleep(0);
+                    }
+                }
+            }
+        }
+
+        public void stilcw2(int l, int mod)
+        {
+             for (int i = 0; i < 256; i+=16)
+            {
+                for (int j = 0; j < 256; j+=16)
+                {
+                    for (int k = 0; k < l; k+=8)
+                    {
+                        try
+                        {
+                            if (mod == 0) { userControl11.g.FillRectangle(new SolidBrush(Color.FromArgb(i, j, k)), (i + l) - 15, (j + l) - 15, 5 + l / 2, 5 + l / 2); }
+                           if(mod==1){ userControl11.g.FillRectangle(new SolidBrush(Color.FromArgb(j, i, k)), i + l, j + l, l - 1, l - 1);}
+                           if(mod==2){ userControl11.g.FillRectangle(new SolidBrush(Color.FromArgb(i, i, k)), i + l, j + l, l - 1, l - 1);}
+                           if (mod == 3) { userControl11.g.FillRectangle(new SolidBrush(Color.FromArgb(j, j, k)), i + l, j + l, l - 1, l - 1); }
+                        }
+                        catch { }
+                        Thread.Sleep(0);
+                    }
+                }
+            }
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            for (int ii = 1; ii <256; ii *= 17)
+            {
+                stilcw2(ii,0);
+                Thread.Sleep(0);
+            }
+            for (int ii = 1; ii < 256; ii *= 17)
+            {
+               // stilcw2(ii, 1);
+                Thread.Sleep(0);
+            }
+            for (int ii = 1; ii < 256; ii *= 17)
+            {
+                //stilcw2(ii, 2);
+                Thread.Sleep(0);
+            }
+            for (int ii = 1; ii < 256; ii *= 17)
+            {
+               // stilcw2(ii, 3);
+                Thread.Sleep(0);
+            }
+
+        }
+
+        private void userControl11_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        static Color GetPixel(Point p)
+        {
+            //merge si nu prea merge
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(p, new Point(0, 0), new Size(1,1));
+                   
+                }
+                return bitmap.GetPixel(0, 0);
+            }
+
+        }
+        private void userControl11_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Text = e.X.ToString() + ":" + e.Y.ToString();
+            Point p = new Point(e.X, e.Y);
+            Color c = GetPixel(p);
+            curentcolor = c;
+            panel1.BackColor = c;
+            Text += c.ToString();
+        }
+
+        private void userControl11_MouseDown(object sender, MouseEventArgs e)
+        {
+            Text = e.X.ToString() + ":" + e.Y.ToString();
+            Point p = new Point(e.X, e.Y);
+            Color c = GetPixel(p);
+            selectedcolor = c;
+            panel2.BackColor = c;
+            Text += c.ToString();
+        }
+    }
+}
